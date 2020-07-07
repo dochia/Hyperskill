da385d44141208bcfdd590a985e1f399eb9c205d
dochia <alexandrae.iacob@gmail.com>
2020-07-07 16:59:07 -0700

    Finishing Encryption/Decryption project

---
+++ Encryption-Decryption/Problems/Advanced calculator/src/Main.java
@@ -0,0 +1,8 @@
+/* Please, do not rename it */
+class Problem {
+
+    public static void main(String[] args) {
+        String operator = args[0];
+        // write your code here
+    }
+}
---
+++ Encryption-Decryption/Problems/Parameters and their values/src/Main.java
@@ -0,0 +1,6 @@
+class Problem {
+
+    public static void main(String[] args) {
+        // Write your code here
+    }
+}
